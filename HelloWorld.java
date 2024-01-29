12ab2b830ca00ee66a3e39a2bf524828d2e1d820
Gun Woo Kim <97578165+Quinsh@users.noreply.github.com>
2024-01-30 08:54:52 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ class HelloWorld {
     public static void main(String args[]) {
         PrintWriter pen = new PrintWriter(System.out, true);
         pen.println("Hello, GitHub! Editted!");
+        pen.println("Exercise 12");
         pen.flush();
     }
 }
